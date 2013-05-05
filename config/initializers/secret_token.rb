44e603c88e2611def915f336eea95ee11d0cb8d2
Ryan Taylor <rtaylor@sessionm.com>
2013-05-04 22:54:16 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bostonedmcalendar::Application.config.secret_token = '7b1f6822392019efd090c63fe8fb22260964a6ea903f006ff11fbc23bd632f366a85bb85d111736f806f8284e3b061d6631562037d21c312ddeab3a4f50d5545'
