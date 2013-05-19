2709129b96753516bc2b5d49417df9d4c7f9eeba
Ryan Taylor <rmt@bu.edu>
2013-05-18 22:46:35 -0400

    Added a comma to check debugging

--- app/helpers/nokogiri_helper.rb
+++ app/helpers/nokogiri_helper.rb
@@ -5,12 +5,7 @@ module NokogiriHelper
   def wantickets
     array = []
     url = "http://wantickets.com/events/default.aspx?zoneId=3307914&pgNum="
-#    record = '.EventRecord.DataRecord'
-#    artist = '.MainEventInfo > .EventInfo > a > span'
-#    '.MainEventInfo > .EventInfo > a > span'
-#    '.VenueInfo'
-#    '.VenueInfo > a > span'
-#    '.DateInfo > .Date > time'
+
     page = Nokogiri::HTML(open(url + "#{1}"))
     page.css('.EventRecord.DataRecord').each do |event|
       array << {
@@ -18,7 +13,7 @@ module NokogiriHelper
         :event_url => event.css('.MainEventInfo > .EventInfo > a > span')[0]['href'],
         :venue => event.css('.VenueInfo > a > span')[0].text.strip,
         :venue_wantickets_id => event.css('.VenueInfo > a')[0]['href'].scan(/\/Venues\/(\d+)/).flatten[0],
-        :date => Date.strptime(event.css('.DateInfo > .Date > time').children[2].text, '%m/%d/%Y')
+        :date => Date.strptime(event.css('.DateInfo > .Date > time').children[2].text, '%m/%d/%Y'),
       }
     end
   end
