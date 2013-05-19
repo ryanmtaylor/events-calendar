c578ebb3b4528cfe7369a1e72482a3df8f210b75
Ryan Taylor <rmt@bu.edu>
2013-05-18 22:40:40 -0400

    Fixed bug in nokogiri_helper

--- app/helpers/nokogiri_helper.rb
+++ app/helpers/nokogiri_helper.rb
@@ -17,8 +17,8 @@ module NokogiriHelper
         :artist => event.css('.MainEventInfo > .EventInfo > a > span').text.strip,
         :event_url => event.css('.MainEventInfo > .EventInfo > a > span')[0]['href'],
         :venue => event.css('.VenueInfo > a > span')[0].text.strip,
-        :venue_wantickets_id => event.css('.VenueInfo > a')[0]['href'].scan(/\/Venues\/(\d+)/).flatten[0]
-        :date => Date.strptime event.css('.DateInfo > .Date > time').children[2].text, '%m/%d/%Y'
+        :venue_wantickets_id => event.css('.VenueInfo > a')[0]['href'].scan(/\/Venues\/(\d+)/).flatten[0],
+        :date => Date.strptime(event.css('.DateInfo > .Date > time').children[2].text, '%m/%d/%Y')
       }
     end
   end
