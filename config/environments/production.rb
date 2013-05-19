d296e36e45ca8680d1550c2cb47ea6cb128522fb
Ryan Taylor <rmt@bu.edu>
2013-05-19 11:02:57 -0400

    Don't precompile assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Bostonedmcalendar::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #don't precompile assets
+  config.assets.initialize_on_precompile = false
 end
