e1ba4fed9ab3736dc818a1488e65b5ca091322bc
Ryan Taylor <mailryan@gmail.com>
2013-07-23 16:26:58 -0400

    Removed extra fields from new event form

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Bostonedmcalendar::Application.routes.draw do
 
   authenticated :user do
     root :to => 'events#index'
+    match '/admin', :to => 'events#index'
     resources :twitters, :users, :events, :venues
   end
 
   #match 'all_new{.}json'#, :to => 'venues#all_new', :as => :all_new_venues_path
   #TODO: post 'import_export.json', => 'venues#import_export.json', :as => :"import_export_venues.json"#, :content_type => :"application/json"
   root :to => "home#index"
-  match '/admin', :to => 'home#admin'
   match '/concerts', :to => 'home#index'
 end
